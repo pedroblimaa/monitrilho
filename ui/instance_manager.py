1dab65287d6adc961b4faea75d611cf75616e23d
Pedro Bonfilio <pedroblimaa@gmail.com>
2023-09-28 19:54:21 -0300

    Fix lock file not working always

--- ui/instance_manager.py
+++ ui/instance_manager.py
@@ -2,22 +2,27 @@ import os
 import sys
 import atexit
 from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtCore import QLockFile  # import QLockFile class
 
+
+LOCK_FILE_PATH = os.path.join(os.path.expanduser("~"), "monitrilho.lock")
 
 class InstanceManager():
     def __init__(self):
         super().__init__()
-
-        self.lock_file_path = os.path.join(os.path.expanduser("~"), ".monitrilho_lock")
         self.check_single_instance()
 
     def check_single_instance(self):
-        if os.path.exists(self.lock_file_path):
+        self.lock_file = QLockFile(self.lock_file_path)  # create a lock file object
+
+        if not self.lock_file.tryLock():  # try to lock the file
             self.show_alert("Another instance is already running.")
             sys.exit(1)
         else:
+            self.lock_file.unlock()  # unlock the file
             with open(self.lock_file_path, "w") as lock_file:
-                lock_file.write(str(os.getpid()))
+                lock_file.write(str(os.getpid()))  # write the PID to the file
+            self.lock_file.lock()
 
         atexit.register(self.remove_lock_file)
 
@@ -29,4 +34,5 @@ class InstanceManager():
         msg_box.exec_()
 
     def remove_lock_file(self):
-        os.remove(self.lock_file_path)
+        self.lock_file.unlock()
+        os.remove(self.lock_file_path)  # remove the fil
